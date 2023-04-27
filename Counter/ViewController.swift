88622216ff38d79dbf51eb14716934a673721d56
Vladimir S <heyvovka@mail.ru>
2023-04-27 23:51:41 +0700

    Сделан счетчик и добавлена верстка

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countZero: UILabel!
+    @IBOutlet weak var buttonCount: UIButton!
+    
+    var count = 0
+    
+    func letsCount() {
+        count += 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func incrementCount(_ sender: Any) {
+        self.letsCount()
+        
+        let countResult = "Значение счётчика: " + "\(count)"
+        countZero.text = countResult
+    }
+    
 }
 
